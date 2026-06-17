917840812bdce5cb877aa67d531bed81c3ba029a
DVRSTxNEON <pierremorales1607@gmail.com>
2026-06-17 14:51:11 -0500

    Creación de data.sql para insertar datos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO docentes (numero_documento, tipo_documento, nombre, apellido, correo, especialidad, anios_experiencia)
+VALUES
+('12345678', 'CC', 'Carlos',   'Pérez',    'cperez@uni.edu.co',    'Ingeniería de Software', 10),
+('87654321', 'CC', 'María',    'González', 'mgonzalez@uni.edu.co', 'Base de Datos',          8),
+('11223344', 'CE', 'Jorge',    'Ramírez',  'jramirez@uni.edu.co',  'Redes',                  5),
+('99887766', 'TI', 'Lucía',    'Torres',   'ltorres@uni.edu.co',   'Matemáticas',            3);
